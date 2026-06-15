f6560b2fdd2591b4259aa109984481e847c9472c
Taufik Januar <taufikjanuar35@gmail.com>
2026-06-15 11:15:46 +0700

    feat: add SUBSCRIPTION_FEATURES_CACHE_KEY constant

--- backend/pkg/constants/cache_key.go
+++ backend/pkg/constants/cache_key.go
@@ -8,5 +8,6 @@ const (
 	DEPARTMEN_CACHE_KEY       = "department:all"
 	SHIFT_CACHE_KEY           = "shift:all"
 	LEAVE_TYPE_CACHE_KEY      = "leave_type:all"
-	COMPANY_PROFILE_CACHE_KEY = "company:profile:%d"
+	COMPANY_PROFILE_CACHE_KEY      = "company:profile:%d"
+	SUBSCRIPTION_FEATURES_CACHE_KEY = "subscription:features:%d"
 )
